e25df82c6c27dbc428e2d7b566a7f9d471a4ca02
mauricio parra <mauparra80@hotmail.com>
2024-06-26 13:40:44 -0500

    added logs

--- src/components/functions/spotifyManager/spotifyAPI.js
+++ src/components/functions/spotifyManager/spotifyAPI.js
@@ -1,9 +1,13 @@
 
 
 const authorizeSpotifyUser = async () => {
+  console.log('Starting authorizeSpotifyUser function');
   try {
+    console.log('Fetching /.netlify/functions/authorizeUser');
     const response = await fetch('/.netlify/functions/authorizeUser');
+    console.log('Response received');
     if (!response.ok) {
+      console.error('Network response was not ok')
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
