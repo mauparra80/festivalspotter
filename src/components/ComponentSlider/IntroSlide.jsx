4619287279e827be24c2cb1f5df8f7a782172a2b
Mauricio Parra <mauparra80@hotmail.com>
2024-07-11 15:45:21 -0500

    fix UI sizing for 1080 resolution

--- src/components/ComponentSlider/IntroSlide.jsx
+++ src/components/ComponentSlider/IntroSlide.jsx
@@ -7,7 +7,7 @@ export default function IntroSlide({step, description, img}) {
     <>
     <h1 className="font-poetsen pb-5">{step}</h1>
     <img src={img} alt="" className='intro-slide-img'/>
-    <p className="text-4xl pt-5 font-poetsen">{description}</p>
+    <p className="pt-5">{description}</p>
     </>
   )
 }
